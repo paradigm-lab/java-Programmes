b5063539b565f112a4fc180ec7d9adb1056f79b5
collins <collinsboniface1000@gmail.com>
2021-11-18 06:58:30 +0200

    Creating an object for the plastic Bag and we print the price by using the getPrice method

--- src/main/java/bag_Sales_System/driver.java
+++ src/main/java/bag_Sales_System/driver.java
@@ -10,12 +10,21 @@ public class driver {
         -> Set selling price according to size, materials and tax
     */
 
+        System.out.println("\t \t The Bag sales system. \n");
+
         //We never make the constructor for any of the class, so we use the default constructor
         paperBag Paperbag = new paperBag();
 
         Paperbag.setLength(10.0);
         Paperbag.setWidth(10.0);
-        System.out.println(Paperbag.getPrice());
+        System.out.println("The price for the Paper Bag: " + Paperbag.getPrice());
+
+        PlasticBag plasticBag = new PlasticBag();
+        plasticBag.setLength(40.0);
+        plasticBag.setWidth(10.0);
+        plasticBag.setTax(0.2);
+        System.out.println("The tax is: " + plasticBag.getTax() + " The total price is: " + plasticBag.getPrice());
+
 
     }
 }
