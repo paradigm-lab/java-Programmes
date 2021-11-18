e399a4f7fe3a7e983c1c432f684f0b1dd26bc603
collins <collinsboniface1000@gmail.com>
2021-11-18 05:55:08 +0200

    Adding comment to the driver.java class

--- src/main/java/average_stock_price/driver.java
+++ src/main/java/average_stock_price/driver.java
@@ -9,11 +9,14 @@ public class driver {
             ->Update the share capital
             ->Update the average price of stock
         */
-
+        //Creating an object for the stock class
         Stock abcCompany = new Stock();
+
+        //Using the stock object to use the method in the stock class
         abcCompany.buy(36.6, 2000);
         abcCompany.buy(39.7, 3000);
         abcCompany.sell(42.9, 1000);
+
         System.out.println(abcCompany.getShares());
         System.out.println(abcCompany.getPrice());
         System.out.println(abcCompany.getAveragePrice());
