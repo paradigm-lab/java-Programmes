c8500c63363f19b019f3d159b8f9ee41f5fdb74d
collins <collinsboniface1000@gmail.com>
2021-11-18 05:53:36 +0200

    writting a program for the stock exchange and updating the build.gradlew that i created the class path that wasn't found at the runntime of the java programme

--- src/main/java/average_stock_price/Stock.java
+++ src/main/java/average_stock_price/Stock.java
@@ -1,4 +1,32 @@
 package average_stock_price;
 
 public class Stock {
+    private int shares;
+    private double price;
+    private double capital;
+
+    //p means the price and s means the number of share
+    public void buy(double p, int s) {
+        shares += s;
+        price = p;
+        capital += s * p;
+    }
+
+    public void sell(double p, int s){
+        shares -= s;
+        price = p;
+        capital -= s * p;
+    }
+
+    public int getShares(){
+        return shares;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public double getAveragePrice(){
+        return capital / getShares();
+    }
 }
--- src/main/java/average_stock_price/driver.java
+++ src/main/java/average_stock_price/driver.java
@@ -10,6 +10,13 @@ public class driver {
             ->Update the average price of stock
         */
 
+        Stock abcCompany = new Stock();
+        abcCompany.buy(36.6, 2000);
+        abcCompany.buy(39.7, 3000);
+        abcCompany.sell(42.9, 1000);
+        System.out.println(abcCompany.getShares());
+        System.out.println(abcCompany.getPrice());
+        System.out.println(abcCompany.getAveragePrice());
 
     }
 }
