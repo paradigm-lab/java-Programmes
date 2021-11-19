34ab61266e1dc4797eda6a33e802bada29a23ea3
collins <collinsboniface1000@gmail.com>
2021-11-19 05:57:44 +0200

    Adding comment to the main method

--- src/main/java/Bank_account/bank.java
+++ src/main/java/Bank_account/bank.java
@@ -1,5 +1,13 @@
 package Bank_account;
 
 public class bank {
+    public static void main(String[] args) {
+        /*
+            Bank Account with transfer
+            -> Create a BankAccount object
+            -> A bank account and sava and withdraw money
+            -> It can transfer money to other accounts
+        */
+    }
 
 }
