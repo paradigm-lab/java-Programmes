164fe36474c300372c0a817b2016ee7357f38cbc
collins <collinsboniface1000@gmail.com>
2021-11-19 06:13:34 +0200

    Updating the file by importing the java class for the gradlew to run

--- src/main/java/AllProgram.java
+++ src/main/java/AllProgram.java
@@ -1,4 +1,5 @@
-import bag_Sales_System.driver;
+import Bank_account.bank;
+//import bag_Sales_System.driver;
 //import average_stock_price.driver;
 
 import projectatm.ATM;
@@ -8,6 +9,7 @@ public class AllProgram{
 	public static void main(String[] args) {
 		//ATM.main(args);
 		//driver.main(args);
-		driver.main(args);
+		//driver.main(args);
+		bank.main(args);
 	}
 }
