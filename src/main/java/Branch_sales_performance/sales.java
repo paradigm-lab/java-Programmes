ceaaf20124317f7d8d36dbab9b993565859511ac
collins <collinsboniface1000@gmail.com>
2021-11-21 11:10:29 +0200

    Updating the sales class by adding the reading of the file to calculate the sum of the yearlly total and each column in the file

--- src/main/java/Branch_sales_performance/sales.java
+++ src/main/java/Branch_sales_performance/sales.java
@@ -1,7 +1,39 @@
 package Branch_sales_performance;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class sales {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter file name, including the extension: ");
+        String filename = scan.nextLine();
+        scan.close();
+        File myFile = new File(filename);
+        scan = new Scanner(myFile);
+
+        //Yearly sum for all branches
+        double yearlySum = 0;
+        //quarterly sum for all branches
+        //initialized to zero by default
+        double[] quarterlySum = new double[4];
+
+        int count = 0;
+        while (scan.hasNextDouble()) {
+            double sales = scan.nextDouble();
+            yearlySum += sales;
+
+            // Quarter 0, 1, 2 and 3
+            int quarter = count % 4;
+            quarterlySum[quarter] += sales;
+            count++;
+        }
+
+        scan.close();
+        System.out.println(yearlySum);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(quarterlySum[i]);
+        }
 
     }
 }
