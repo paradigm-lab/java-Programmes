357595e93f7a8050a24460522c23036acea91efa
collins <collinsboniface1000@gmail.com>
2021-11-21 12:14:55 +0200

    Adding a new Compare students marks package and the new java class that has the main method and adding the comment in the main method

---
+++ src/main/java/Compare_students_marks/marks.java
@@ -0,0 +1,16 @@
+package Compare_students_marks;
+
+public class marks {
+    public static void main(String[] args) {
+        /*
+        Compare Student marks
+        -> Check which student has a higher total marks
+        -> Use compareTo method from Comparable interface
+        -> Instantiate Student objects
+        -> Get subjects and marks for each object
+        -> Compare th total marks of 2 students
+         */
+
+
+    }
+}
