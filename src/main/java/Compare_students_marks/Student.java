62fe601b501fd40b521cf369a4e5f3a125c88199
collins <collinsboniface1000@gmail.com>
2021-11-21 14:39:24 +0200

    Creating the two objects for the student class and using the method to addmarks and compareTo method to compare the two students

--- src/main/java/Compare_students_marks/Student.java
+++ src/main/java/Compare_students_marks/Student.java
@@ -1,4 +1,44 @@
 package Compare_students_marks;
 
-public class Student {
+import java.util.ArrayList;
+
+public class Student implements Comparable<Student> {
+
+    private int totalMark;
+    private String name;
+    private ArrayList<String> subjects;
+    private ArrayList<Integer> marks;
+
+    //The constructor for the student class
+    public Student(String name) {
+        this.name = name;
+        subjects = new ArrayList<String>();
+        marks = new ArrayList<Integer>();
+    }
+
+    //The getter method for the name
+    public String getName() {
+        return name;
+    }
+
+    //The addMarks method
+    public void addMarks(String subject, int mark) {
+        subjects.add(subject);
+        marks.add(mark);  //Autoboxing done for us
+        totalMark = totalMark + mark;  //Initialization has been done during instantiation
+    }
+
+    public int compareTo(Student other) {
+        if (this.totalMark > other.totalMark) {
+            return 1;
+        }
+        else if (this.totalMark == other.totalMark) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+
 }
--- src/main/java/Compare_students_marks/marks.java
+++ src/main/java/Compare_students_marks/marks.java
@@ -11,6 +11,25 @@ public class marks {
         -> Compare th total marks of 2 students
          */
 
+        Student collin = new Student("Collin");
+        collin.addMarks("Computer Programming", 95);
+        collin.addMarks("Mathematics", 100);
+        collin.addMarks("Computer Hacking", 98);
+
+        Student Matt = new Student("Mathew");
+        Matt.addMarks("Computer Programming", 100);
+        Matt.addMarks("Mathematics", 100);
+        Matt.addMarks("Computer Hacking", 99);
+
+        if(collin.compareTo(Matt) > 0) {
+            System.out.println(collin.getName() + " gets a higher total mark");
+        }
+        else if (collin.compareTo(Matt) < 0) {
+            System.out.println(Matt.getName() + " gets a higher total marks");
+        }
+        else {
+            System.out.println("Both students get the same total mark");
+        }
 
     }
 }
