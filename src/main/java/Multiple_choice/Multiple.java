f759196c23b983bed9057a4db140522053c8bc42
collins <collinsboniface1000@gmail.com>
2021-11-21 14:46:57 +0200

    Creating the new package and adding the Multiple class with the main method and the comment in the main method

---
+++ src/main/java/Multiple_choice/Multiple.java
@@ -0,0 +1,18 @@
+package Multiple_choice;
+
+public class Multiple {
+
+    public static void main(String[] args){
+        /*
+                Multiple-choice test
+                -> Prepare a question set with answer
+                -> Display questions randomly
+                -> Display choices randomly
+                -> Put questions into a Game instance
+                -> Record number of correct answer of that Game instance
+         */
+
+
+    }
+
+}
