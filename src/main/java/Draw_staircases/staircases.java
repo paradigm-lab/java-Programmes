37868b86728614e7edf03014bb1660b37f295766
collins <collinsboniface1000@gmail.com>
2021-11-22 09:26:07 +0200

    Creating a new Draw staircases package and making a new staircases class and adding the comment into the class

---
+++ src/main/java/Draw_staircases/staircases.java
@@ -0,0 +1,12 @@
+package Draw_staircases;
+
+public class staircases {
+    /*
+     Draw staircases
+     -> Given number of steps and step width
+     -> Draw steps using for loops
+     -> Steps = 5 and width = 3
+     */
+
+
+}
