4fdaa3e51a87988ec10347cbf6449b92f443a077
collins <collinsboniface1000@gmail.com>
2021-11-22 10:50:06 +0200

    Adding the fields and the method in the sub class will the CommercialDriver extends to the GenericDriver class

--- src/main/java/Driving_license_system/CommercialDriver.java
+++ src/main/java/Driving_license_system/CommercialDriver.java
@@ -1,4 +1,25 @@
 package Driving_license_system;
 
-public class CommercialDriver {
+public class CommercialDriver extends GenericDriver{
+    private String hazardousMaterialsType;
+    private String carType; // Special commercial car category
+
+    public CommercialDriver(String name, boolean isProvisional, String materialType, String carType) {
+        super(name, isProvisional);
+        hazardousMaterialsType = materialType;
+        this.carType = carType;
+    }
+
+    //The getter method for the
+    public String getMaterialType() {
+        return hazardousMaterialsType;
+    }
+
+    //Override the GenericDriver's method
+    @Override
+    public void setMedicalReport(){
+        medicalReport = "Approved to drive " + carType;
+    }
+
+
 }
