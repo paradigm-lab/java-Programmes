919860edd9721af4a09f08e03b0da67d04aa7ab7
collins <collinsboniface1000@gmail.com>
2021-11-22 09:38:50 +0200

    Printing the staircase in the 5 steps and width of 3

--- src/main/java/Draw_staircases/staircases.java
+++ src/main/java/Draw_staircases/staircases.java
@@ -7,6 +7,19 @@ public class staircases {
      -> Draw steps using for loops
      -> Steps = 5 and width = 3
      */
+    public static void main(String[] args) {
+        int numSteps = 5;
+        int stepWidth = 3;
 
+        //Printing the rows
+        for (int i = 0; i < numSteps; i++) {
+            //Printing the column
+            for (int j = 0; j < (i + 1) * stepWidth; j++) {
+                System.out.print("*");
+            }
+            // Change the row
+            System.out.println();
+        }
 
+    }
 }
