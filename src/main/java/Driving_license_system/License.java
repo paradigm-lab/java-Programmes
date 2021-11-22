4836f7bdec5e6763df83203961e2c3e5b26f7dcd
collins <collinsboniface1000@gmail.com>
2021-11-22 09:45:57 +0200

    Adding the new comment in the License class of the main method

--- src/main/java/Driving_license_system/License.java
+++ src/main/java/Driving_license_system/License.java
@@ -3,6 +3,15 @@ package Driving_license_system;
 public class License {
 
     public static void main(String[] args) {
+        /*
+        Driving License System
+        -> Create GenericDriver Class
+        -> Derive CommercialDriver class
+            -Different medical report formats
+            -can transport hazardous materials
+        -> Method overriding is needed
+         */
+
 
     }
 }
