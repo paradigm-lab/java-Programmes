9104cef263a52ec9406667c1842de4dad85cb8f2
collins <collinsboniface1000@gmail.com>
2021-11-22 10:39:48 +0200

    Adding the fields and the method in the generic driver class

--- src/main/java/Driving_license_system/GenericDriver.java
+++ src/main/java/Driving_license_system/GenericDriver.java
@@ -1,4 +1,51 @@
 package Driving_license_system;
 
 public class GenericDriver {
+    private String name;
+    private int points;
+    private boolean isProvisional;
+    // Both superclass and subclass can access it
+    protected String medicalReport;
+
+    //The constructor of the GenericDriver
+    public GenericDriver(String name, boolean isProvisional) {
+        this.name = name;
+        points = 15;
+        this.isProvisional = isProvisional;
+    }
+
+    //The getter method
+    public String getName() {
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public boolean getIsProvisional() {
+        return isProvisional;
+    }
+
+    public String getMedicalReport() {
+        return medicalReport;
+    }
+
+    public void setMedicalReport() {
+        medicalReport = "Approved to drive private car";
+    }
+
+    public void deduct(int p){
+        // Check if negative point can result
+        if (points >= p) {
+            points -= p;
+        }
+        else {
+            points = 0;
+        }
+
+        if (points == 0) {
+            System.out.println("License Suspended");
+        }
+    }
 }
