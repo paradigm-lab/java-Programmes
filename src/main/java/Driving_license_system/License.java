de90aae3d2d3f6fb244dcdb5ebc582f06dc83df8
collins <collinsboniface1000@gmail.com>
2021-11-22 11:05:03 +0200

    Creating the class object into the main method and it worked as expected

--- src/main/java/Driving_license_system/License.java
+++ src/main/java/Driving_license_system/License.java
@@ -11,7 +11,25 @@ public class License {
             -can transport hazardous materials
         -> Method overriding is needed
          */
+        GenericDriver tom = new GenericDriver("Tom", true);
+        tom.deduct(3);
+        tom.setMedicalReport();
 
+        CommercialDriver Jerry = new CommercialDriver("Jerry", false, "Inflammable", "truck");
+        Jerry.deduct(5);
+        Jerry.setMedicalReport();
+
+        GenericDriver[] driverArray = new GenericDriver[2];
+        driverArray[0] = tom;
+        driverArray[1] = Jerry; // Upcast to GenericDriver
+
+        for (int i = 0; i < driverArray.length; i++) {
+            System.out.println(driverArray[i].getName() + "'s license is provisional: " + driverArray[i].getIsProvisional());
+            System.out.println(driverArray[i].getName() + " has " + driverArray[i].getPoints() + " points");
+            System.out.println(driverArray[i].getMedicalReport());
+        }
+
+        System.out.println(Jerry.getMaterialType());
 
     }
 }
