fb81713e1fc0cae2f4bc4ab157e9365bebe95170
collins <collinsboniface1000@gmail.com>
2021-11-23 06:42:23 +0200

    Adding the investement earnings package and Creating the new earnings class in the package

---
+++ src/main/java/Investement_earnings/earnings.java
@@ -0,0 +1,14 @@
+package Investement_earnings;
+
+public class earnings {
+    public static void main(String[] args) {
+        /*
+        Investment earning
+        -> Enter the buying price per share
+        -> Enter closing price on subsequent days
+        -> Calculate aggregate earnings/losses after each
+        -> Leave program when closing price is negative
+        */
+
+    }
+}
