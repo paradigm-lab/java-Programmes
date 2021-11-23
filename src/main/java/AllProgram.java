a81597080e8728f51933927c8ab8c5a834e9d3b2
collins <collinsboniface1000@gmail.com>
2021-11-23 06:36:12 +0200

    Updating the AllProgram class by adding the main class

--- src/main/java/AllProgram.java
+++ src/main/java/AllProgram.java
@@ -6,7 +6,8 @@ import Compare_students_marks.marks;
 //import projectatm.ATM;
 //import Branch_sales_performance.sales;
 //import Draw_staircases.staircases;
-import Driving_license_system.License;
+//import Driving_license_system.License;
+import Game_of_dice.dice;
 //import Multiple_choice.Multiple;
 
 public class AllProgram{
@@ -20,7 +21,8 @@ public class AllProgram{
 		//marks.main(args);
 		//Multiple.main(args);
 		//staircases.main(args);
-		License.main(args);
+		//License.main(args);
+		dice.main(args);
 
 	}
 }
