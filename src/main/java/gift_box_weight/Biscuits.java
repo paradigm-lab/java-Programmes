94ee44f12063e99dcd3e6c03cdef1ce395470915
collins <collinsboniface1000@gmail.com>
2021-11-24 19:11:01 +0200

    Adding the instance variable, constructor and the getter method in the Chocolate and Biscuits class

--- src/main/java/gift_box_weight/Biscuits.java
+++ src/main/java/gift_box_weight/Biscuits.java
@@ -1,4 +1,13 @@
 package gift_box_weight;
 
 public class Biscuits {
+    private double weight;
+
+    public Biscuits(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
--- src/main/java/gift_box_weight/Chocolate.java
+++ src/main/java/gift_box_weight/Chocolate.java
@@ -1,4 +1,19 @@
 package gift_box_weight;
 
 public class Chocolate {
+    private double weight;
+    private String flavor;
+
+    public Chocolate(double weight, String flavor) {
+        this.weight = weight;
+        this.flavor = flavor;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
 }
