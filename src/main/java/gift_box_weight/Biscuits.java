c87b87813bea74a8891796e4d24152460bf6d5cf
collins <collinsboniface1000@gmail.com>
2021-11-24 19:06:17 +0200

    Creating a new package and adding the java classes in the package

---
+++ src/main/java/gift_box_weight/Biscuits.java
@@ -0,0 +1,4 @@
+package gift_box_weight;
+
+public class Biscuits {
+}
---
+++ src/main/java/gift_box_weight/gift.java
@@ -0,0 +1,14 @@
+package gift_box_weight;
+
+public class gift {
+    public static void main(String[] args) {
+        /*
+        Gift box Weight checker
+        -> Create Biscuits, Chocolate instance
+        -> Put a number of these instance into a GiftBox instance using ArrayLists
+        -> Check if the gift box is overweight
+            -> Return the content of the overweight gift boxes
+         */
+
+    }
+}
